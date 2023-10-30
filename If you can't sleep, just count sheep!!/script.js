0025bdf304457bc7385382b3939fc107070ce702
Fat <100190343+Fahatmah@users.noreply.github.com>
2023-10-30 14:23:42 +0800

    Create script.js

---
+++ If you can't sleep, just count sheep!!/script.js
@@ -0,0 +1,7 @@
+const countSheep = (num) => {
+  let sheeps = '' // to store the output
+//   using the num to output the num base on its length 
+  for (let i = 1; i <= num; i++) sheeps += i + ' sheep...'
+//   if the num is 0 then return empty string else the sheeps
+  return num === 0 ? '' : sheeps
+}
