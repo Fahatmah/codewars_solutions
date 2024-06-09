1d5ee00e3d8e91f4a4bab9420b9c22d5d7c5888d
Fahatmah <fahatmahmabang9@gmail.com>
2024-06-09 12:55:59 +0800

    8 kyu solutions

---
+++ Formatting Decimal Places #0/script.js
@@ -0,0 +1,16 @@
+// const twoDecimalPlaces = (n) => {
+//   return parseFloat(n.toFixed(2))
+// }
+
+const twoDecimalPlaces = (n) => {
+  return +n.toFixed(2)
+}
+
+// const twoDecimalPlaces = (n) => {
+//   return Number(n.toFixed(2))
+// }
+
+console.log(twoDecimalPlaces(4.659725356)) // 4.66, "didn't work for 4.659725356");
+console.log(twoDecimalPlaces(173735326.3783732637948948)) // 173735326.38, "didn't work for 173735326.3783732637948948");
+console.log(twoDecimalPlaces(4.653725356)) // 4.65, "didn't work for 4.653725356");
+console.log(twoDecimalPlaces(4.659725356))
